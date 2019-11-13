e6d35a968bc0edb8db949284d9c60667b37cb7b8
kimsooyoung <tge1375@naver.com>
2019-11-13 18:27:49 +0900

    Updates

---
+++ precache-manifest.226173ab789f6d66ee623a83b366f8b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02a8da3d5b8eab7c8f62d80d57ede16b",
+    "url": "/birthday-congraz/index.html"
+  },
+  {
+    "revision": "bbd7b4349346a80bd946",
+    "url": "/birthday-congraz/static/css/main.341e33d1.chunk.css"
+  },
+  {
+    "revision": "e116c06812d9bd6c39ba",
+    "url": "/birthday-congraz/static/js/2.b4d03f33.chunk.js"
+  },
+  {
+    "revision": "bbd7b4349346a80bd946",
+    "url": "/birthday-congraz/static/js/main.4d4ae505.chunk.js"
+  },
+  {
+    "revision": "316ed9b99d9ed8996741",
+    "url": "/birthday-congraz/static/js/runtime-main.fd3c87bf.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/birthday-congraz/static/media/logo.25bf045c.svg"
+  }
+]);
