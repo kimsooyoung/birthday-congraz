2a32129c6ac759ebd2a15a35eda40cdda89a8400
kimsooyoung <tge1375@naver.com>
2019-11-13 18:20:18 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Disqus from 'disqus-react';
 
-function App() {
+const App = () => {
+  const disqusShortname = "happy-b-day" //found in your Disqus.com dashboard
+  const disqusConfig = {
+    url: "http://localhost:3000", //this.props.pageUrl
+    identifier: "article-id", //this.props.uniqueId
+    title: "Title of Your Article" //this.props.title
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div> 
+    <div className="article-container">
+
+      <h1>Article Title</h1>
+
+      <p>Article content.</p>
+
+      <Disqus.DiscussionEmbed
+        shortname={disqusShortname}
+        config={disqusConfig}
+      />
     </div>
   );
 }
